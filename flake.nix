c74b16241edbde72e5b7b9da8c98eee83f8a5667
BCJones1225 <bcjones1225@gmail.com>
2025-04-10 14:44:34 -0500

    On branch master Changes to be committed: 	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,28 @@
 {
-  description = "A very basic flake";
+  description = "System, User, and dotfile configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+      system = "x86_64-linux";
     in {
       nixosConfigurations = {
         nixos = lib.nixosSystem {
-          system = "x86_64-linux";
+          inherit system;
           modules = [ ./configuration.nix ];
+        };
+      };
+      homeConfigurations = {
+        bryancj = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [./home.nix ];
         };
       };
     };
